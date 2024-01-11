c91f0077fb13b685070d3b8a042aa4df309dc6f3
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-01-11 15:18:54 -0500

    mouse addition

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,38 @@
-//potions moving 
+//potions moving
 document.addEventListener("mousemove", parallax);
-    function parallax(e){
-      document.querySelectorAll(".object").forEach(function(move){
+function parallax(e) {
+  document.querySelectorAll(".object").forEach(function (move) {
+    var moving_value = move.getAttribute("data-value");
+    var x = (e.clientX * moving_value) / 250;
+    var y = (e.clientY * moving_value) / 250;
 
-        var moving_value = move.getAttribute("data-value");
-        var x = (e.clientX * moving_value) / 250;
-        var y = (e.clientY * moving_value) / 250;
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
+const cursor = document.querySelector(".cursor");
+var timeout;
 
-        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
-      });
-    }
+//follow cursor on mousemove
+document.addEventListener("mousemove", (e) => {
+  //let x = e.pageX;
+  //let y = e.pageY;
+
+  let x = e.clientX; //replace pageX for clientX
+  let y = e.clientY; // replace  pageY for clientY
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  //cursor effects on mouse stopped
+  function mouseStopped() {
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+//cursor effects on mouseout
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
